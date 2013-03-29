33ad9a306f593561545ba01e3afbab610101c1a4
unknown <seaki@seaki-PC.(none)>
2013-03-29 20:03:25 +0900

    apply all of my dns settings

--- sastudio.jp.lua
+++ sastudio.jp.lua
@@ -5,10 +5,21 @@
 -- _a = "example.com"
 
 -- A records
-a(concat("luadns", _a), "101.1.144.99")
+a(concat("seaki-server", _a), "101.1.144.99")
+a(concat("blog", _a), "219.94.163.224")	-- sakuratan
+a(concat("portage.gentoo", _a), "59.106.174.89")
+
+-- AAAA records
+aaaa(concat("seaki-server", _a), "2001:380:e0a:db::1")
+aaaa(concat("portage.gentoo", _a), "2001:e41:3b6a:ae59::1")
 
 -- CNAME records
-cname(concat("www.luadns", _a), concat("luadns", _a))
+cname(concat("www", _a), concat("seaki-server", _a))
+cname(concat("*", _a), concat("seaki-server", _a))
+cname(concat("@", _a), concat("seaki-server", _a))
+
+-- MX records
+mx(_a, concat("a.mx", _a), 10, 3600)
 
 -- Templates (see templates.lua)
 -- google_app(_a)
