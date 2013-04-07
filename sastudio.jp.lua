dd45b4193a5ee6ce329d9c0416cb97649fa1dfde
seaki <se-aki-personal@sastudio.jp>
2013-04-07 18:06:27 +0900

    Removed CNAME(_a,"seaki-server") .

--- sastudio.jp.lua
+++ sastudio.jp.lua
@@ -5,18 +5,19 @@
 -- _a = "example.com"
 
 -- A records
+a(_a, "101.1.144.99")
 a(concat("seaki-server", _a), "101.1.144.99")
 a(concat("blog", _a), "219.94.163.224")	-- sakuratan
 a(concat("portage.gentoo", _a), "59.106.174.89")
 
 -- AAAA records
+aaaa(_a, "2001:380:e0a:db::1")
 aaaa(concat("seaki-server", _a), "2001:380:e0a:db::1")
 aaaa(concat("portage.gentoo", _a), "2001:e41:3b6a:ae59::1")
 
 -- CNAME records
 cname(concat("www", _a), concat("seaki-server", _a))
 cname(concat("*", _a), concat("seaki-server", _a))
-cname(_a, concat("seaki-server", _a))
 
 -- MX records
 mx(_a, concat("a.mx", _a), 10, 3600)
